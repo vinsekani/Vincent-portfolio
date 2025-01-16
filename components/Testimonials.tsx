a66120cf444702ea7a144d07e14d2f6f9d164d8d
vinsekani <vinsekani@gmail.com>
2025-01-16 09:58:22 +0300

    the testimonial section is already done but i want to add some sliders

---
+++ components/Testimonials.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TestimonialSlider from "./TestimonialSlider";
+
+const Testimonials = () => {
+  return (
+    <div className="bg-[#02050a] pt-[4rem] md:pt-[8rem]">
+      <h1 className="heading">
+        Client <span className="text-yellow-400">Review</span>
+      </h1>
+      <div className="pt-[5rem] pb-[4rem] w-[80%] mx-auto">
+        {/* TESTIMONIAL SLIDER */}
+        <TestimonialSlider />
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
